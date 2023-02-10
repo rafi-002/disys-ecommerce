7cd4d94148285594947dcb402d79edff88ca51dd
Mohamed Rafi <rafimohamed98@gmail.com>
2023-02-10 07:31:39 +0530

    Initial commit for e-commerce website

---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+const products = [
+    {
+        name: "iPhone",
+        description: "Bery slow phone",
+        
+    },
+    {
+        name: "Samsung galaxy ",
+        description: "Bery sweet",
+        
+    },
+    {
+        name: "Oneplus 11",
+        description: "Outcast, Bing chilling",
+        
+    },
+    {
+        name: "Nokia 3310",
+        description: "Thor's hammer",
+        
+    },
+    {
+        name: "Barbie phone",
+        description: "Chinese ringtone",
+        
+    }
+
+]
+
+let productsDiv = document.getElementsByClassName("products-div");
+for(let p of products){
+    let products = document.date
+}
